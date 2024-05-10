68bb301b5a45657073b141ac24da4d0d882f3fe4
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 20:31:25 +0300

    create authentication backend

---
+++ fastapi-application/api/dependencies/backend.py
@@ -0,0 +1,10 @@
+from fastapi_users.authentication import AuthenticationBackend
+
+from core.authentication.transport import bearer_transport
+from .strategy import get_database_strategy
+
+authentication_backend = AuthenticationBackend(
+    name="access-tokens-db",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)
