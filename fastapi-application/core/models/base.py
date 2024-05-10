f2cc24afb12f2368adf82f66978f3dcb72491563
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 19:53:15 +0300

    create id int pk mixin

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -18,5 +18,3 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
