7803a6aa8283192bff51a15db736572bcaf9bbba
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 21:24:05 +0300

    add http bearer dependency for tokens

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends,
+)
+from fastapi.security import HTTPBearer
 
 from core.config import settings
 
 from .auth import router as auth_router
 from .users import router as users_router
 
+
+http_bearer = HTTPBearer(auto_error=False)
+
 router = APIRouter(
     prefix=settings.api.v1.prefix,
+    dependencies=[Depends(http_bearer)],
 )
 router.include_router(auth_router)
 router.include_router(users_router)
