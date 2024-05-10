d0903b50775684bd3a8d6e06a0890eebbda54b37
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 20:20:51 +0300

    create AccessToken config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -36,6 +36,10 @@ class DatabaseConfig(BaseModel):
     }
 
 
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),
@@ -46,6 +50,7 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+    access_token: AccessToken = AccessToken()
 
 
 settings = Settings()
