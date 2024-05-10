9fc9c195bb9764119daaf77338e24fe245dc83e2
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 20:45:53 +0300

    create users schemas

---
+++ fastapi-application/core/schemas/user.py
@@ -0,0 +1,15 @@
+from fastapi_users import schemas
+
+from core.types.user_id import UserIdType
+
+
+class UserRead(schemas.BaseUser[UserIdType]):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
