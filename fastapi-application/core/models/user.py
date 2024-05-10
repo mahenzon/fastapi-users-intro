6ac42c57db59d07dd9611551bfdb3f61ab5cf232
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 20:16:28 +0300

    fix user model imports

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,10 +1,9 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users.db import (
+from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
 )
-
 from core.types.user_id import UserIdType
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
