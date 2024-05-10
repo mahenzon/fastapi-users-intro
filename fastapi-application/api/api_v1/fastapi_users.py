cc9ec77525bc8572db2cdfb52fecec32b5b422da
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-10 20:57:59 +0300

    move fastapi users routers helper to routers package

--- fastapi-application/api/api_v1/fastapi_users.py
+++ fastapi-application/api/api_v1/fastapi_users.py
@@ -3,8 +3,8 @@ from fastapi_users import FastAPIUsers
 from core.models import User
 from core.types.user_id import UserIdType
 
-from .user_manager import get_user_manager
-from .backend import authentication_backend
+from api.dependencies.authentication.user_manager import get_user_manager
+from api.dependencies.authentication.backend import authentication_backend
 
 fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
