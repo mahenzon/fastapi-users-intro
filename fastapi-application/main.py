ec558553babe2a6368e28ebb5e34808a9f0a71d6
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-02-02 16:27:09 +0300

    add create_app util

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,35 +1,20 @@
 import logging
-from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
-
 from core.config import settings
 
 from api import router as api_router
-from api.webhooks import webhooks_router
-from core.models import db_helper
+from create_fastapi_app import create_app
 
 logging.basicConfig(
     level=settings.logging.log_level_value,
     format=settings.logging.log_format,
 )
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup
-    yield
-    # shutdown
-    await db_helper.dispose()
-
-
-main_app = FastAPI(
-    default_response_class=ORJSONResponse,
-    lifespan=lifespan,
-    webhooks=webhooks_router,
+main_app = create_app(
+    create_custom_static_urls=True,
 )
+
 main_app.include_router(
     api_router,
 )
