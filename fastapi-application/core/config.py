3270f4384e81d6fe4df1db9091c655b11c615e0a
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-02-22 15:08:08 +0300

    always absolute paths

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Literal
+from pathlib import Path
 
 from pydantic import BaseModel
 from pydantic import PostgresDsn
@@ -8,6 +9,7 @@ from pydantic_settings import (
     SettingsConfigDict,
 )
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOG_DEFAULT_FORMAT = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
@@ -86,7 +88,10 @@ class AccessToken(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=(
+            BASE_DIR / ".env.template",
+            BASE_DIR / ".env",
+        ),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
