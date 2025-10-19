8d5f36f7439bd858bbcd9ad551981cf84ef6d735
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-10-19 19:36:08 +0300

    add get_users_list view

--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from typing import TYPE_CHECKING, Annotated
 
+from fastapi import APIRouter, Depends
+
+from api.dependencies.authentication import get_users_db
 from core.authentication.fastapi_users import fastapi_users
 from core.config import settings
 from core.schemas.user import (
@@ -7,10 +10,29 @@ from core.schemas.user import (
     UserUpdate,
 )
 
+if TYPE_CHECKING:
+    from core.models import User
+    from core.models.user import SQLAlchemyUserDatabase
+
 router = APIRouter(
     prefix=settings.api.v1.users,
     tags=["Users"],
 )
+
+
+@router.get(
+    "",
+    response_model=list[UserRead],
+)
+# async def get_users_list() -> list[UserRead]:
+async def get_users_list(
+    users_db: Annotated[
+        "SQLAlchemyUserDatabase",
+        Depends(get_users_db),
+    ],
+) -> list["User"]:
+    return await users_db.get_users()
+
 
 # /me
 # /{id}
