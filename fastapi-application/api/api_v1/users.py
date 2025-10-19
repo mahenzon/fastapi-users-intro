8a4be42a7c52bb2aa512045c0e888aa57ee77f1a
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-10-19 19:51:11 +0300

    create cache for users list with custom key builder

--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -1,10 +1,15 @@
 from typing import TYPE_CHECKING, Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, Request
+from fastapi_cache.decorator import cache
+
+import hashlib
+from typing import Any, Callable, Dict, Optional, Tuple
 
 from api.dependencies.authentication import get_users_db
 from core.authentication.fastapi_users import fastapi_users
 from core.config import settings
+from core.models.user import SQLAlchemyUserDatabase
 from core.schemas.user import (
     UserRead,
     UserUpdate,
@@ -12,7 +17,6 @@ from core.schemas.user import (
 
 if TYPE_CHECKING:
     from core.models import User
-    from core.models.user import SQLAlchemyUserDatabase
 
 router = APIRouter(
     prefix=settings.api.v1.users,
@@ -20,11 +24,38 @@ router = APIRouter(
 )
 
 
+def users_list_key_builder(
+    func: Callable[..., Any],
+    namespace: str,
+    *,
+    request: Optional[Request] = None,
+    response: Optional[Response] = None,
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    exclude_types = (SQLAlchemyUserDatabase,)
+    cache_kw = {}
+    for name, value in kwargs.items():
+        if isinstance(value, exclude_types):
+            continue
+        cache_kw[name] = value
+
+    cache_key = hashlib.md5(  # noqa: S324
+        f"{func.__module__}:{func.__name__}:{args}:{cache_kw}".encode()
+    ).hexdigest()
+    return f"{namespace}:{cache_key}"
+
+
 @router.get(
     "",
     response_model=list[UserRead],
 )
 # async def get_users_list() -> list[UserRead]:
+@cache(
+    expire=60,
+    key_builder=users_list_key_builder,
+    namespace=settings.cache.namespace.users_list,
+)
 async def get_users_list(
     users_db: Annotated[
         "SQLAlchemyUserDatabase",
