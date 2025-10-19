73767ab5f2bf76b521615f43e110f3523cc88620
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-10-19 19:35:57 +0300

    add get_users method to SQLAlchemyUserDatabase

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -2,8 +2,9 @@ from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy import (
     SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase,
+    SQLAlchemyUserDatabase as SQLAlchemyUserDatabaseGeneric,
 )
+from sqlalchemy import select
 from sqlalchemy.orm import relationship, Mapped
 
 from core.types.user_id import UserIdType
@@ -13,6 +14,14 @@ from .mixins.id_int_pk import IdIntPkMixin
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
     from core.models import AccessToken
+
+
+class SQLAlchemyUserDatabase(SQLAlchemyUserDatabaseGeneric):
+
+    async def get_users(self) -> list["User"]:
+        statement = select(User).order_by(User.id)
+        results = await self.session.scalars(statement)
+        return list(results.all())
 
 
 class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
